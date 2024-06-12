f19dbe7739fc9ccc8f4625c68dbc6e10fcec5424
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2024-06-12 17:56:53 +0200

    fix: format lua config
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek.98@gmail.com>

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,11 +6,10 @@ local config = wezterm.config_builder()
 
 config.keys = {
     -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
-    {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+    { key = "LeftArrow", mods = "OPT", action = wezterm.action { SendString = "\x1bb" } },
     -- Make Option-Right equivalent to Alt-f; forward-word
-    {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
-  }
+    { key = "RightArrow", mods = "OPT", action = wezterm.action { SendString = "\x1bf" } },
+}
 
 -- and finally, return the configuration to wezterm
 return config
-
