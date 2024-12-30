f2456dce1bd9cda4fab15843197a1008f2bfe2fc
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2024-12-30 13:43:18 +0100

    fix: do some minor cleanups
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek.98@gmail.com>

--- .environment.sh
+++ .environment.sh
@@ -1,2 +1,2 @@
-export PATH="/opt/homebrew/bin:${PATH}:${HOME}/go/bin:${HOME}/.local/bin:${HOME}/.krew/bin"
+export PATH="/opt/homebrew/opt/findutils/libexec/gnubin:/opt/homebrew/bin:${PATH}:${HOME}/go/bin:${HOME}/.local/bin:${HOME}/.krew/bin"
 export EDITOR="nvim"
