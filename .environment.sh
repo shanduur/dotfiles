e79a0022698f8d6b799aa44f9d5bdd64f79376d2
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2025-05-14 10:18:57 +0200

    feat: add few changes
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek.98@gmail.com>

--- .environment.sh
+++ .environment.sh
@@ -1,13 +1,14 @@
-export PATH="/opt/homebrew/bin:${PATH}:${HOME}/go/bin:${HOME}/.local/bin:${HOME}/.krew/bin"
-export EDITOR="nvim"
-
 BREW_BIN="/usr/local/bin/brew"
 if [ -f "/opt/homebrew/bin/brew" ]; then
     BREW_BIN="/opt/homebrew/bin/brew"
 fi
 
+export PATH="${PATH}:${HOME}/go/bin:${HOME}/.local/bin:${HOME}/.krew/bin"
+export EDITOR="nvim"
+
 if type "${BREW_BIN}" &> /dev/null; then
     export BREW_PREFIX="$("${BREW_BIN}" --prefix)"
+    export PATH="${BREW_PREFIX}/bin:${PATH}:${HOME}/go/bin:${HOME}/.local/bin:${HOME}/.krew/bin"
     for bindir in "${BREW_PREFIX}/opt/"*"/libexec/gnubin"; do export PATH=$bindir:$PATH; done
     for bindir in "${BREW_PREFIX}/opt/"*"/bin"; do export PATH=$bindir:$PATH; done
     for mandir in "${BREW_PREFIX}/opt/"*"/libexec/gnuman"; do export MANPATH=$mandir:$MANPATH; done
